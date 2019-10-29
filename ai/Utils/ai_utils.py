126f067ef50c3bf83b975c70bdaac266a176703b
di3cruz <diegocruz967@gmail.com>
2019-10-29 18:25:40 -0500

    Modified function fn_label_encoder

--- ai/Utils/ai_utils.py
+++ ai/Utils/ai_utils.py
@@ -6,20 +6,20 @@ from sklearn.preprocessing import LabelEncoder
 '''-----------------------------------------------------------'''
 
 
-'''--------------------- Encoder label --------'''
-
-# returns the variable to target numerically
+'''--------------------- Encoder & decoded label --------'''
+# Return the variable to target numerically
+# 1) Fit: object_encoder: fn_label_encoder(df,"name_column")
+# 2) transform labels: y_encoded = object_encoder.transform(df["name_column"])
+# 3) Decoded target:  y_decoded = object_encoder.inverse_transform([number code])
 
 
 def fn_label_encoder(df, name_column):
+    le = LabelEncoder()
     try:
-        # Encoder label target
-        le = LabelEncoder()
-        y = le.fit_transform(df[name_column])
-        return y
+        le.fit(df[name_column])
+        return le
     except Exception as e:
         print("Error in function fn_label_encoder")
         print("type error: " + str(e))
         print(traceback.format_exc())
         exit()
-

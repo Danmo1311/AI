33d47aaa2a193ac53cb95e5f6041e09db59abbce
di3cruz <diegocruz967@gmail.com>
2019-11-17 17:38:32 -0500

    Object detection yolo

---
+++ ai/Object_detection_realtime/object_detection_yolo.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+import time
+
+# Importamos el modelo pre entrenado
+model = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
+
+# Asignamos los nombres de las clases pre entrenadas
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# Obtenemos los nombres de las capas
+layer_names = model.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in model.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Cargamos la camara
+camera = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_PLAIN
+starting_time = time.time()
+frame_id = 0
+while True:
+    _, frame = camera.read()
+    frame_id += 1
+    height, width, channels = frame.shape
+    while True:
+        _, frame = camera.read()
+        frame_id += 1
+        height, width, channels = frame.shape
+        # Deteccion de objetos
+        blob = cv2.dnn.blobFromImage(frame, 0.00392, (416,416), (0, 0, 0), True, crop=False)
+        model.setInput(blob)
+        outs = model.forward(output_layers)
+        # Informacion que aparecera en pantalla
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.2:
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.3)
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                confidence = confidences[i]
+                color = colors[class_ids[i]]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + 30), color, -1)
+                cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 30), font, 3, (255,255,255), 3)
+        elapsed_time = time.time() - starting_time
+        fps = frame_id / elapsed_time
+        cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10, 50), font, 3, (0, 0, 0), 3)
+        cv2.imshow("Diego Cruz", frame)
+        # Oprimiendo la tecla "q" finalizamos el proceso
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    camera.release()
+    cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ############## Encender la camara
+# while(True):
+# # Capture frame-by-frame
+#     ret,frame = cap.read()
+# # Display the resulting frame
+#     cv2.imshow("preview",frame)
+#     #Waits for a user input to quit the application
+#     if cv2.waitKey(1) & 0xFF == ord("q"):
+#         break
+

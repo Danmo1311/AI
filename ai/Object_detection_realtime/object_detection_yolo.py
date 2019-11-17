8c429eb282334540300e7376d37dc0927bacfb64
di3cruz <diegocruz967@gmail.com>
2019-11-17 17:43:42 -0500

    Object detection yolo.

--- ai/Object_detection_realtime/object_detection_yolo.py
+++ ai/Object_detection_realtime/object_detection_yolo.py
@@ -72,31 +72,3 @@ while True:
             break
     camera.release()
     cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- ############## Encender la camara
-# while(True):
-# # Capture frame-by-frame
-#     ret,frame = cap.read()
-# # Display the resulting frame
-#     cv2.imshow("preview",frame)
-#     #Waits for a user input to quit the application
-#     if cv2.waitKey(1) & 0xFF == ord("q"):
-#         break
-
